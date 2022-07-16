d6bb90fd27cf1db3be790a39a58ac1264f0fdd81
akmalov <akmalovkomoliddin505@gmail.com>
2022-07-16 14:03:03 +0500

    first

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'postgres:host=ec2-3-217-14-181.compute-1.amazonaws.com;dbname=demo9futqnattv',
+    'username' => 'xxofylssvfqsdi',
+    'password' => '98ad1425a6a328bb5df66566cefc8d809dc444ffc490839916ef744acd5a937b',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
